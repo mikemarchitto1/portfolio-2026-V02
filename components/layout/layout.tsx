5eece643b37ad4311bd8d5a4bab3621d80ef8fed
Trees <carlwalker@Carls-MacBook-Pro.local>
2026-02-22 18:55:50 -0500

    Fix LayoutProps type error

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,4 +1,6 @@
-export default function Layout({ children }: LayoutProps) {
+import { ReactNode } from "react";
+
+export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="w-full max-w-[1328px] mx-auto">
       {children}
