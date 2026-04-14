45317bec25b4516800d3666771627f202a08db2a
Mike Marchitto <mikemarchitto@Mikes-MacBook-Pro.local>
2026-04-14 19:39:29 -0400

    Update

--- components/hero-section/hero-section.tsx
+++ components/hero-section/hero-section.tsx
@@ -22,7 +22,7 @@ export default function HeroSection() {
                 </h4>
                 <div className="text-body1 space-y-4 mt-7">
                   <p>
-                  I’m from Chicago, where I studied graphic design at UIC, a program rooted in Swiss and International design principles. That foundation shaped my early approach to visual storytelling. After moving to Seattle, I shifted into UX design and worked across corporations, agencies, and startups, learning how research creates more authentic user experiences. I’m now based in Miami, balancing product design with AI‑driven exploration.
+                  I’m from Chicago, where I studied graphic design at UIC, a program rooted in Swiss and International design principles that shaped my early approach to visual storytelling. After moving to Seattle, I shifted into UX design and worked across corporations, agencies, and startups, learning how research drives more authentic user experiences. I’m now based in Miami, balancing product design with ongoing AI‑driven exploration.
                   </p>
                 </div>
               </div>
