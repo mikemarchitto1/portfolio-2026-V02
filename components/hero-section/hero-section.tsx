426fa4baee0fbd71e133b63cbd70321285a83d87
Mike Marchitto <mikemarchitto@Mikes-MacBook-Pro.local>
2026-06-09 19:09:24 -0400

    Update

--- components/hero-section/hero-section.tsx
+++ components/hero-section/hero-section.tsx
@@ -50,7 +50,7 @@ export default function HeroSection() {
                       03
                     </span>
                     <p className="text-body1 text-foreground leading-relaxed">
-                    Accelerate delivery with AI‑augmented research, IA, and prototyping automation.
+                    Accelerate delivery with AI‑augmented research, IA, rapid prototyping and automation.
                     </p>
                   </div>
 
